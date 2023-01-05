47ebc1c210bc6e44f27c406b92cb232722452527
Gonzalo Iurman <gonpwn@gmail.com>
2023-01-05 18:50:35 -0300

    DarkMode

--- script.js
+++ script.js
@@ -1,15 +1,27 @@
 const buyButton = document.getElementById("buyButton");
 const back = document.getElementById("backButton");
-// const imgHero = document.getElementById("heroImg");
-
+const ligthMode = document.getElementById("lightMode");
+const ligthMode2 = document.getElementById("lightMode2");
 const image = document.getElementById("heroImg");
+const hero = document.getElementById("heroSection");
+const links = document.querySelector("link");
 
 let t1 = gsap.timeline({ paused: true, reversed: true });
 let t2 = gsap.timeline({ paused: true, reversed: true });
+let t3 = gsap.timeline({ paused: true, reversed: true });
 
 buyButton.addEventListener("click", () => {
-  console.log("asdasd");
   t2.play();
+});
+
+ligthMode.addEventListener("click", () => {
+  hero.style.color = "#ffffff";
+  t3.play();
+});
+
+ligthMode2.addEventListener("click", () => {
+  hero.style.color = "black";
+  t3.reverse();
 });
 
 back.addEventListener("click", () => {
@@ -152,6 +164,41 @@ t2.to(
     scale: 8,
     width: 350,
     duration: 0.3,
+  },
+  0.3
+);
+
+//LightMode TimeLine
+
+t3.to(
+  ".darkMode",
+  {
+    ease: "power1.inOut",
+    scale: 100,
+    width: 1200,
+    height: 1200,
+    duration: 1,
+    backgroundColor: "#232323",
+  },
+  0.3
+);
+
+t3.to(
+  ".darkMode i",
+  {
+    ease: "power1.inOut",
+    scale: 0,
+    duration: 0.3,
+  },
+  0
+);
+
+t3.to(
+  ".lightMode",
+  {
+    ease: "elastic.out(1, .8)",
+    right: 20,
+    duration: 1,
   },
   0.3
 );
